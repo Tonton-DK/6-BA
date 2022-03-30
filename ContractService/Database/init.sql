0808828f7ae0953d18e3510ef52d32bc7d1bd4e8
Jacob <jacob-dan@hotmail.com>
2022-03-30 18:16:03 +0200

    feat: Add init.sql

--- ContractService/Database/init.sql
+++ ContractService/Database/init.sql
@@ -2,9 +2,14 @@ CREATE DATABASE test;
 USE test;
 
 CREATE TABLE Contract (
-  UserID CHAR(36) PRIMARY KEY,
-  Firstname NVARCHAR(500) NOT NULL,
-  Lastname NVARCHAR(500) NOT NULL
+  ID CHAR(36) PRIMARY KEY,
+  JobId CHAR(36) NOT NULL,
+  OfferId CHAR(36) NOT NULL,
+  ClientId CHAR(36) NOT NULL,
+  ProviderId CHAR(36) NOT NULL,
+  CreationDate DATETIME NOT NULL,
+  ClosedDate DATETIME,
+  State ENUM ('Open', 'Concluded', 'Cancelled') NOT NULL
 );
 
 ALTER user 'root' IDENTIFIED WITH mysql_native_password BY '';
