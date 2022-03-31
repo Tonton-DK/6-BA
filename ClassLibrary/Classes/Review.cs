239ef48bf1483fc9c63dc53b727bea7107abb987
Jacob <jacob-dan@hotmail.com>
2022-03-31 22:05:28 +0200

    feat: Update Review class

--- ClassLibrary/Classes/Review.cs
+++ ClassLibrary/Classes/Review.cs
@@ -2,7 +2,25 @@
 
 public class Review
 {
-    
+    public Review(Guid id, Guid contractId, Guid creatorId, Guid targetId, string comment, int rating, ReviewType type)
+    {
+        Id = id;
+        ContractId = contractId;
+        CreatorId = creatorId;
+        TargetId = targetId;
+        Comment = comment;
+        Rating = rating;
+        Type = type;
+    }
+
+    public Guid Id { get; set; }
+    public Guid ContractId { get; set; }
+    public Guid CreatorId { get; set; }
+    public Guid TargetId { get; set; }
+    public string Comment { get; set; }
+    public int Rating { get; set; }
+    public ReviewType Type { get; set; }
+
 }
 
 public enum ReviewType
