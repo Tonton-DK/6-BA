0c4bf5ecc611010f563a6e73c3a8b6826e31e583
Jacob <jacob-dan@hotmail.com>
2022-04-11 17:43:04 +0200

    feat: Base setup

---
+++ Frontend/Pages/Login.cshtml.cs
@@ -0,0 +1,12 @@
+$HEADER$using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace $NAMESPACE$
+{
+    public class $CLASS$ : PageModel
+    {
+        public void OnGet()
+        {
+	          $END$
+        }
+    }
+}
