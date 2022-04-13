4aec19543bef61145940fe5dda2442e3fa6bca4c
Jacob <jacob-dan@hotmail.com>
2022-04-13 22:16:38 +0200

    feat: Redirect to frontpage

--- Frontend/Pages/CreateOffer.cshtml.cs
+++ Frontend/Pages/CreateOffer.cshtml.cs
@@ -9,46 +9,40 @@ public class CreateOfferModel : PageModel
 {
     private readonly ILogger<CreateOfferModel> _logger;
     
-    private readonly IUserService _userService;
-    private readonly IJobService _jobService;
     private readonly IOfferService _offerService;
     
     public Dictionary<Type, bool> ServiceStatus { get; private set; }
     
-    public User Client { get; private set; }
-    public User Provider { get; private set; }
-    public Job Job { get; private set; }
-    public Offer? PreviousOffer { get; private set; }
     public Offer Offer { get; private set; }
     
-    public CreateOfferModel(ILogger<CreateOfferModel> logger, 
-        IUserService userService, 
-        IJobService jobService, 
+    public CreateOfferModel(ILogger<CreateOfferModel> logger,
         IOfferService offerService)
     {
         _logger = logger;
-        _userService = userService;
-        _jobService = jobService;
         _offerService = offerService;
         ServiceStatus = new Dictionary<Type, bool>();
     }
     
-    public void OnGet()
-    {
-        //TODO: Set Instance variables OnGet
-    }
-    
+    [BindProperty]
+    public Guid JobId { get; set; }
+    [BindProperty]
+    public Guid ProviderId { get; set;}
     [BindProperty]
     public int Price { get; set; }
     [BindProperty]
     public string Duration { get; set;}
     [BindProperty]
     public DateTime Date { get; set;}
-    
-    public void OnPost()
+
+    public IActionResult OnGet()
     {
-        Offer = new Offer(Guid.Empty, Guid.Empty, Guid.Empty, Price, Duration, Date, State.Open);
+        return Page();
+    }
+    
+    public async Task<IActionResult> OnPost()
+    {
+        Offer = new Offer(Guid.Empty, JobId, ProviderId, Price, Duration, Date, State.Open);
         _offerService.CreateOffer(Offer);
-        RedirectToPage("/Job?id="+Offer.JobId);
+        return RedirectToPage("Index");
     }
 }
