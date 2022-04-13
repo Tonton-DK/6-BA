d8df26775e6e51d9372f4646901e525377def30b
Jacob <jacob-dan@hotmail.com>
2022-04-13 22:28:15 +0200

    feat: Basic HTML setup

--- Frontend/Pages/CreateUser.cshtml.cs
+++ Frontend/Pages/CreateUser.cshtml.cs
@@ -1,9 +1,27 @@
+using ClassLibrary.Classes;
+using ClassLibrary.Interfaces;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Frontend.Pages;
 
-public class CreateUser : PageModel
+public class CreateUserModel : PageModel
 {
+    private readonly ILogger<CreateUserModel> _logger;
+    
+    private readonly IUserService _userService;
+    
+    public Dictionary<Type, bool> ServiceStatus { get; private set; }
+    
+    public User User { get; private set; }
+    
+    public CreateUserModel(ILogger<CreateUserModel> logger,
+        IUserService userService)
+    {
+        _logger = logger;
+        _userService = userService;
+        ServiceStatus = new Dictionary<Type, bool>();
+    }
+    
     public void OnGet()
     {
         
