6f78dbf1e5f96f34c821f8dd18a13d196ac52684
Jacob <jacob-dan@hotmail.com>
2022-04-14 00:58:53 +0200

    feat: Add fields

--- Frontend/Pages/CreateJob.cshtml.cs
+++ Frontend/Pages/CreateJob.cshtml.cs
@@ -1,11 +1,29 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using ClassLibrary.Classes;
+using ClassLibrary.Interfaces;
 
 namespace Frontend.Pages;
 
-public class CreateJob : PageModel
+public class CreateJobModel : PageModel
 {
+    private readonly ILogger<CreateJobModel> _logger;
+    
+    private readonly IJobService _jobService;
+    
+    public Dictionary<Type, bool> ServiceStatus { get; private set; }
+    
+    public Job Job { get; private set; }
+    
+    public CreateJobModel(ILogger<CreateJobModel> logger,
+        IJobService jobService)
+    {
+        _logger = logger;
+        _jobService = jobService;
+        ServiceStatus = new Dictionary<Type, bool>();
+    }
     public void OnGet()
     {
         
     }
+    
 }
