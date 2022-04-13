f7db179529bfaa054147e4ae2fceaa2765f72fe3
anton <antontj@live.dk>
2022-04-13 15:09:04 +0200

    Testing local MySQL instance for unit tests

--- ContractService/Tests/Test.cs
+++ ContractService/Tests/Test.cs
@@ -2,6 +2,8 @@
 using ContractService.Controllers;
 using ContractService.Interfaces;
 using Moq;
+using MySql.Data.MySqlClient;
+using MySql.Server;
 using NUnit.Framework;
 
 namespace ContractService.Tests;
@@ -11,6 +13,31 @@ public class Test
     [SetUp]
     public void Setup()
     {
+    }
+
+    [Test]
+    public void DBTest()
+    {
+        MySqlServer database = MySqlServer.Instance;
+        database.StartServer();
+
+        var cs = database.GetConnectionString();
+        using var con = new MySqlConnection(cs);
+        con.Open();
+        
+        var sql = "SELECT 1";
+        using var cmd = new MySqlCommand(sql, con);
+        
+        var result = Convert.ToInt32(cmd.ExecuteScalar());
+        
+        database.ShutDown();
+        Assert.AreEqual(1, result); 
+        
+        /*
+        var sql = "UPDATE Contract SET Contract.JobId = @jobId, Contract.OfferId = @offerId, Contract.ClientId = @clientId, Contract.ProviderId = @providerId, Contract.CreationDate = @creationDate, Contract.ClosedDate = @closedDate, Contract.State = @state WHERE Contract.ID = @id";
+        using var cmd = new MySqlCommand(sql, con);
+        cmd.ExecuteNonQuery();
+        */
     }
     
     [Test]
