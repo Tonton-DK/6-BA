ddc25f2dac3d606bb8a69683414bb4580acfcfdb
anton <antontj@live.dk>
2022-04-14 16:20:33 +0200

    db fix

--- UserService/Database/init.sql
+++ UserService/Database/init.sql
@@ -12,7 +12,7 @@ CREATE TABLE User (
   IsServiceProvider BIT NOT NULL
 );
 
-INSERT INTO User(ID, Email, Password, Firstname, Lastname, PhoneNumber, IsServiceProvider) VALUES("156be3a6-5537-41f8-9608-705c7cd7cbc3", "test@mail.dk", "secret", "John", "Doe", "12345678", 0);
+INSERT INTO User(ID, Email, PasswordSalt, PasswordHash, Firstname, Lastname, PhoneNumber, IsServiceProvider) VALUES("156be3a6-5537-41f8-9608-705c7cd7cbc3", "test@mail.dk", "s7eu/Mz30bvsZnjSBot7gg==", "O+3jrM6+EudEJ3WwNS67aFqEi5eztupag3u+YG3Es64=", "John", "Doe", "12345678", 0);
 
 ALTER user 'root' IDENTIFIED WITH mysql_native_password BY '';
 flush privileges;
