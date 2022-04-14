eccd3caebb76b004e3240dbb47d7db4676dcd656
Jacob <jacob-dan@hotmail.com>
2022-04-14 20:38:34 +0200

    feat: Add instance variables

--- Frontend/Pages/ViewUser.cshtml.cs
+++ Frontend/Pages/ViewUser.cshtml.cs
@@ -1,9 +1,36 @@
+using ClassLibrary.Classes;
+using ClassLibrary.Interfaces;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Frontend.Pages;
 
-public class ViewUser : PageModel
+public class ViewUserModel : PageModel
 {
+    private readonly ILogger<ViewUserModel> _logger;
+    
+    private readonly IUserService _userService;
+    private readonly IJobService _jobService;
+    private readonly IContractService _contractService;
+    private readonly IReviewService _reviewService;
+
+    public Dictionary<Type, bool> ServiceStatus { get; private set; }
+
+    public User Client { get; private set; }
+    public IEnumerable<Job> Jobs { get; private set; }
+    public IEnumerable<Contract> OpenContracts { get; private set; }
+    public IEnumerable<Contract> ClosedContracts { get; private set; }
+    
+    public ViewUserModel(ILogger<ViewUserModel> logger,
+        IJobService jobService,
+        IUserService userService,
+        IContractService contractService)
+    {
+        _logger = logger;
+        _jobService = jobService;
+        _userService = userService;
+        _contractService = contractService;
+        ServiceStatus = new Dictionary<Type, bool>();
+    }
     public void OnGet()
     {
         
