7d5c5c982b7d3bd1ad6806366569725bf6158f2a
Jacob <jacob-dan@hotmail.com>
2022-04-14 13:19:20 +0200

    feat: Add jobs list

--- Frontend/Pages/ListTasks.cshtml.cs
+++ Frontend/Pages/ListTasks.cshtml.cs
@@ -1,11 +1,30 @@
+using ClassLibrary.Classes;
+using ClassLibrary.Interfaces;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Frontend.Pages;
 
-public class ListTasksModel : PageModel
+public class ListJobsModel : PageModel
 {
+    private readonly ILogger<ListJobsModel> _logger;
+    
+    private readonly IJobService _jobService;
+    
+    public Dictionary<Type, bool> ServiceStatus { get; private set; }
+
+    public IEnumerable<Job> Jobs { get; private set; }
+    
+    public ListJobsModel(ILogger<ListJobsModel> logger,
+        IJobService jobService)
+    {
+        _logger = logger;
+        _jobService = jobService;
+        ServiceStatus = new Dictionary<Type, bool>();
+    }
+    
     public void OnGet()
     {
-        
+        var filter = new Filter(null, null, null, "", "");
+        Jobs = _jobService.ListJobs(filter);
     }
 }
