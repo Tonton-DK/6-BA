010772f42c964512b671abbf18c64196f1fc1ed0
anton <antontj@live.dk>
2022-04-15 13:23:02 +0200

    smoke testing

--- Frontend/Controllers/HealthCheckController.cs
+++ Frontend/Controllers/HealthCheckController.cs
@@ -1,19 +1,19 @@
 ﻿using ClassLibrary.Interfaces;
-using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc;
 
-namespace Frontend.Pages;
+namespace Frontend.Controllers;
 
-public class ServiceCheckModel : PageModel
+[ApiController]
+[Route("api/[controller]")]
+public class HealthCheckController : ControllerBase
 {
     private readonly IUserService _userService;
     private readonly IJobService _jobService;
     private readonly IOfferService _offerService;
     private readonly IContractService _contractService;
     private readonly IReviewService _reviewService;
-
-    public Dictionary<Type, bool> ServiceStatus { get; private set; }
-
-    public ServiceCheckModel(ILogger<IndexModel> logger, 
+    
+    public HealthCheckController( 
         IUserService userService, 
         IJobService jobService, 
         IOfferService offerService, 
@@ -25,15 +25,17 @@ public class ServiceCheckModel : PageModel
         _offerService = offerService;
         _contractService = contractService;
         _reviewService = reviewService;
-        ServiceStatus = new Dictionary<Type, bool>();
     }
 
-    public void OnGet()
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public IActionResult Get()
     {
-        ServiceStatus.Add(_userService.GetType(), _userService.Get());
-        ServiceStatus.Add(_jobService.GetType(), _jobService.Get());
-        ServiceStatus.Add(_offerService.GetType(), _offerService.Get());
-        ServiceStatus.Add(_contractService.GetType(), _contractService.Get());
-        ServiceStatus.Add(_reviewService.GetType(), _reviewService.Get());
+        _userService.Get();
+        _jobService.Get();
+        _offerService.Get();
+        _contractService.Get();
+        _reviewService.Get();
+        return Ok();
     }
 }
