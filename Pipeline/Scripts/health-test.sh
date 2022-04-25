99d30a93cd95152174140ee4b232d60b0085386f
anton <antontj@live.dk>
2022-04-25 14:22:14 +0200

    better smoke test

--- Pipeline/Scripts/health-test.sh
+++ Pipeline/Scripts/health-test.sh
@@ -7,14 +7,14 @@ while [ $CURRENT_ATTEMPT -le $MAX_ATTEMPTS ]; do
     echo "Testing connection - Attempt: ${CURRENT_ATTEMPT} of ${MAX_ATTEMPTS}"
     RESULT=$(curl -o /dev/null -s -w "%{http_code}\n" http://localhost:8080/api/HealthCheck/)
     
-    if [ $RESULT -ne '200' ]; then
-     echo "Connection didn't return a status code of 200. Connection failed"
-     exit 1
+    if [ $RESULT -e '200' ]; then
+     echo "All connection attempts were successful"
+     exit 0
     fi
 
     CURRENT_ATTEMPT=$((CURRENT_ATTEMPT + 1))
     sleep 1s
 done
 
-echo "All connection attempts were successful"
-exit 0
+echo "Connection didn't return a status code of 200. Connection failed"
+exit 1
