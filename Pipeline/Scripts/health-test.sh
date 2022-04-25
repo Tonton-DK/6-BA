d4fe015c407c5b08aaae0aa81935efd76ebff6c1
anton <antontj@live.dk>
2022-04-25 15:40:09 +0200

    Integration test in pipeline

--- Pipeline/Scripts/health-test.sh
+++ Pipeline/Scripts/health-test.sh
@@ -2,10 +2,13 @@
 
 MAX_ATTEMPTS=10
 CURRENT_ATTEMPT=1
+Endpoint=$1
+
+echo "Testing endpoint: http://localhost:8080/api/HealthCheck/$Endpoint"
 
 while [ $CURRENT_ATTEMPT -le $MAX_ATTEMPTS ]; do
     echo "Testing connection - Attempt: ${CURRENT_ATTEMPT} of ${MAX_ATTEMPTS}"
-    RESULT=$(curl -o /dev/null -s -w "%{http_code}\n" http://localhost:8080/api/HealthCheck/Services)
+    RESULT=$(curl -o /dev/null -s -w "%{http_code}\n" http://localhost:8080/api/HealthCheck/$Endpoint)
     
     if [ $RESULT -eq '200' ]; then
      echo "All connection attempts were successful"
