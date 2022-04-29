318e7d6f2341ddc705868575b65fd4716834930e
Jacob <jacob-dan@hotmail.com>
2022-04-29 14:48:49 +0200

    feat: Fix offer

--- Frontend/Pages/CreateOffer.cshtml.cs
+++ Frontend/Pages/CreateOffer.cshtml.cs
@@ -21,7 +21,6 @@ public class CreateOfferModel : LayoutModel
         _jobService = jobService;
         _offerService = offerService;
         Offer = new Offer();
-        logger.Log(LogLevel.Warning,"Model instantiated");
     }
 
     [BindProperty]
@@ -29,27 +28,23 @@ public class CreateOfferModel : LayoutModel
     
     public Job Job { get; set;}
     
+    public Guid JobId { get; set;}
+    
     public IActionResult OnGet(Guid jobId)
     {
         Instantiate();
-        _logger.Log(LogLevel.Warning, Offer.JobId.ToString());
         
         Job = _jobService.GetJobById(jobId);
-        _logger.Log(LogLevel.Warning, Offer.JobId.ToString());
+        JobId = Job.Id;
         
-        Offer.JobId = Job.Id;
-        _logger.Log(LogLevel.Warning, Offer.JobId.ToString());
         return Page();
     }
     
     public async Task<IActionResult> OnPost()
     {
-        _logger.Log(LogLevel.Warning, Offer.JobId.ToString());
-        
         Instantiate();
-        _logger.Log(LogLevel.Warning, Offer.JobId.ToString());
         
-        Offer.JobId = Job.Id;
+        Offer.JobId = JobId;
         
         //TODO: If any previous offers set provider id to same as this.
         Offer.ProviderId = new Guid(HttpContext.Session.GetString(SessionIdKey));
